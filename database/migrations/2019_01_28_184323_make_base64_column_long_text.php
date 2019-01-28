2584ddd8e8bfcd26f268bad939dbfdd696c0d5d3
rsbertoa90 <rsbertoa90@gmail.com>
2019-01-28 18:46:09 -0300

    longtext for base64 field

---
+++ database/migrations/2019_01_28_184323_make_base64_column_long_text.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeBase64ColumnLongText extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product_images', function (Blueprint $table) {
+            //
+             $table->longText('base64')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product_images', function (Blueprint $table) {
+            $table->dropColumn('base64');
+        });
+    }
+}
