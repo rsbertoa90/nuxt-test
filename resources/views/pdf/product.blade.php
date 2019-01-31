a47608c6bc5a44215b376b9bc0db9be458c5ceda
rsbertoa90 <rsbertoa90@gmail.com>
2019-01-31 18:50:18 -0300

    tststs

--- resources/views/pdf/product.blade.php
+++ resources/views/pdf/product.blade.php
@@ -1,14 +1,16 @@
 
 <td style="width:24%; overflow:hidden; ">                        
     <img style="width:150px; "  src="{{$product->images[0]->tobase()}}"> 
-    <div style="margin-top:-16px; margin-left:10px;">
+    <div style="margin-top:-16px; margin-left:10px;">  <br>
         @if ($product->price > 0)
         
-        <br> <span style="width:73px ; height:60px; font-size: .87rem ; color:blue ; display:inline "> ${{number_format($product->price,2)}} </span>
+             <span style="width:73px ; height:60px; font-size: .87rem ; color:blue ; display:inline "> ${{number_format($product->price,2)}} </span>
+        @endif
+        @if ($product->pck_units > 1 && $product->price > 0)
+            <span> / </span>
         @endif
         @if ($product->pck_units > 1)
-         
-         <span style="width:73px ; height:60px;font-size: .9rem ; color:green ; display:inline ;">/ ${{number_format($product->pck_price,2)}}</span>
+         <span style="width:73px ; height:60px;font-size: .9rem ; color:green ; display:inline ;"> ${{number_format($product->pck_price,2)}}</span>
         @endif
     </div>
    
