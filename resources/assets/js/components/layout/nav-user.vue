da3f5a17fb4c4179bf5324e11f6b228605ded170
rsbertoa90 <rsbertoa90@gmail.com>
2019-03-18 22:19:44 -0300

    detalles

--- resources/assets/js/components/layout/nav-user.vue
+++ resources/assets/js/components/layout/nav-user.vue
@@ -1,7 +1,7 @@
 <template>
          <nav class="navbar navbar-expand-lg navbar-dark bg-fucsia font-weight-bold">
                 <a class="navbar-brand " href="/">
-                    <img style="width:100px" src="/storage/images/app/logo.png" alt="Bazar Mayorista Maju">
+                    <v-lazy-image style="width:100px" src="/storage/images/app/logo.png" alt="Bazar Mayorista Maju" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon text-white"></span>
