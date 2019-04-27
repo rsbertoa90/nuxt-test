b78b900b0bce013f744213c5b8c51e26e1947d17
rsbertoa90 <rsbertoa90@gmail.com>
2019-04-27 19:53:13 -0300

    reduce chunks

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -52,14 +52,14 @@ import "core-js/modules/es6.array.iterator";
 
 const Login = () => import( /* webpackChunkName: "admin-chunk" */  './components/login/Login.vue');
 
-const Home =  import('./components/home/Home.vue') ; 
-const Cotizer =  import('./components/cotizer/Cotizer.vue');
-const Contact =  import('./components/contact/Contact.vue');
-const Cart =  import('./components/shoppingCart/ShoppingCart.vue');
-const Category =  import('./components/category/Category.vue');
-const CategoryIndex =  import('./components/category/categoryIndex.vue');
-const Product =  import('./components/category/product/Product.vue');
-const Offers =  import ('./components/offers/Offers.vue');
+const Home = () =>  import(/* webpackChunkName: "user-chunk" */ './components/home/Home.vue') ; 
+const Cotizer = () => import(/* webpackChunkName: "user-chunk" */ './components/cotizer/Cotizer.vue');
+const Contact = () => import(/* webpackChunkName: "user-chunk" */ './components/contact/Contact.vue');
+const Cart = () => import(/* webpackChunkName: "user-chunk" */ './components/shoppingCart/ShoppingCart.vue');
+const Category = () => import(/* webpackChunkName: "user-chunk" */ './components/category/Category.vue');
+const CategoryIndex = () => import(/* webpackChunkName: "user-chunk" */ './components/category/categoryIndex.vue');
+const Product = () => import(/* webpackChunkName: "user-chunk" */ './components/category/product/Product.vue');
+const Offers = ()  => import (/* webpackChunkName: "user-chunk" */ './components/offers/Offers.vue');
 
 
 const Admin = () => import( /* webpackChunkName: "admin-chunk" */ './components/admin/Admin.vue');
